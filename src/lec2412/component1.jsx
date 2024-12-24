05fb7bd668e17094cb3a1dfcfc06dec3a3c867de
priyal <priyalbhesaniya@gmail.com>
2024-12-24 13:13:58 +0530

    add component

---
+++ src/lec2412/component1.jsx
@@ -0,0 +1,13 @@
+import * as React from "react";
+//each class component must contain a render function
+class Component1 extends React.Component{
+    render() {
+        return(
+            <div>
+                <h1>component 1</h1>
+                <h2> component 2</h2>
+            </div>
+        )
+    }
+}
+export default Component1;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import {createRoot } from 'react-dom/client'
+import {StrictMode } from 'react'
+import Component1 from './lec2412/component1';
 
-
+// 1. class based implementation
+//2 . function based implementation
+//3. jsx javascript xml
+// <DIV> FOR HTML BUT HERE <div> is for react
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    My name is priyal
+   <Component1/>
   </StrictMode>,
 )
