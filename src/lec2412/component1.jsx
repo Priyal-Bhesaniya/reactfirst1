2474bb7718836d61225ce50d74ecf39de20fd344
priyal <priyalbhesaniya@gmail.com>
2024-12-24 13:24:57 +0530

    add component

--- src/lec2412/component1.jsx
+++ src/lec2412/component1.jsx
@@ -1,12 +1,29 @@
 import * as React from "react";
+// in one component we can import  multiple components
+class Component2 extends React.Component
+{
+    render() {
+        return(
+            <h2>Component 2</h2>
+        )
+    }
+}
+class Component3 extends React.Component{
+    render() {
+        return(
+            <h3>Component 3</h3>
+        )
+    }
+}
 //each class component must contain a render function
 class Component1 extends React.Component{
     render() {
         return(
             <div>
                 <h1>component 1</h1>
-                <h2> component 2</h2>
-                <h3> component 3</h3>
+                <h2>hello</h2>
+                <Component2 />
+                <Component3 />
             </div>
         )
     }
