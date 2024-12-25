2c37bc3951acd06728bb69275c9a7af35f2ff3f9
Priyal-Bhesaniya <priyalbhesaniya@gmail.com>
2024-12-25 10:46:57 +0530

    complete a timetable

---
+++ src/lec2412/timetable.jsx
@@ -0,0 +1,128 @@
+import * as React from "react";
+
+class Timetable extends React.Component {
+    render (){
+        return(
+
+            <div className="timetable">
+                <center>
+                <table style={{border:"1px solid black",borderCollapse: "collapse",width:"100%",textAlign:"center"}}>
+                    <thead >
+                    <tr>
+                    <td style={{ border:"1px solid black",padding:"10px"}}  >Time</td> 
+                  <td style={{ border:"1px solid black",padding:"10px"}}  >Monday</td>
+                  <td style={{ border:"1px solid black",padding:"10px"}}>Tuesday</td>
+                  <td style={{ border:"1px solid black",padding:"10px"}}>Wednesday</td>
+                  <td style={{ border:"1px solid black",padding:"10px"}}>Thursday</td>
+                  <td style={{ border:"1px solid black",padding:"10px"}}>Friday</td>
+                    </tr>
+                    </thead>
+                    <tbody >
+                    <tr>
+                    <td style={{ border:"1px solid black",padding:"10px"}}>8:00 AM - 9:00 AM</td>
+                  <td style={{ border:"1px solid black",padding:"10px"}}>Reac(nv)</td>
+                  <td style={{ border:"1px solid black",padding:"10px"}}>Reac(nv)</td>
+                  <td style={{ border:"1px solid black",padding:"10px"}}>Reac(nv)</td>
+                  <td style={{ border:"1px solid black",padding:"10px"}}>Reac(nv)</td>
+
+                  <td style={{ border:"1px solid black",padding:"10px"}}>Reac(nv)</td>
+
+
+                  
+                  </tr>
+                  <tr>
+                  <td style={{ border:"1px solid black",padding:"10px"}}>8:00 AM - 9:00 AM</td>
+                  <td style={{ border:"1px solid black",padding:"10px"}}>Reac(nv)</td>
+                  <td style={{ border:"1px solid black",padding:"10px"}}>Reac(nv)</td>
+                  <td style={{ border:"1px solid black",padding:"10px"}}>Reac(nv)</td>
+                  <td style={{ border:"1px solid black",padding:"10px"}}>Reac(nv)</td>
+
+                  <td style={{ border:"1px solid black",padding:"10px"}}>Reac(nv)</td>
+                  </tr>
+                  <tr>
+                  <td style={{ border:"1px solid black",padding:"10px"}}>8:00 AM - 9:00 AM</td>
+                  <td style={{ border:"1px solid black",padding:"10px"}}>Reac(nv)</td>
+                  <td style={{ border:"1px solid black",padding:"10px"}}>Reac(nv)</td>
+                  <td style={{ border:"1px solid black",padding:"10px"}}>Reac(nv)</td>
+                  <td style={{ border:"1px solid black",padding:"10px"}}>Reac(nv)</td>
+                  <td style={{ border:"1px solid black",padding:"10px"}}>Reac(nv)</td>
+                  </tr>
+                  <tr>
+                  <td style={{ border:"1px solid black",padding:"10px"}}>8:00 AM - 9:00 AM</td>
+                 <td colSpan={5}>TEA BREAK</td>
+                 
+                  </tr>
+                  <tr>
+                    <td style={{ border:"1px solid black",padding:"10px"}}>9:00 AM - 10:00 AM</td>
+                    <td style={{ border:"1px solid black",padding:"10px"}}>Reac(nv)</td>
+                  <td style={{ border:"1px solid black",padding:"10px"}}>Reac(nv)</td>
+                  <td style={{ border:"1px solid black" ,padding:"10px"}} rowSpan={2}>Reac(nv)</td>
+                  <td style={{ border:"1px solid black",padding:"10px"}}>Reac(nv)</td>
+                  <td style={{ border:"1px solid black",padding:"10px"}}>Reac(nv)</td>
+
+                  </tr>
+                  <tr>
+                    <td style={{ border:"1px solid black",padding:"10px"}}>9:00 AM - 10:00 AM</td>
+                    <td style={{ border:"1px solid black",padding:"10px"}}>Reac(nv)</td>
+                  <td style={{ border:"1px solid black",padding:"10px"}}>Reac(nv)</td>
+                 
+                  <td style={{ border:"1px solid black",padding:"10px"}}>Reac(nv)</td>
+                  <td style={{ border:"1px solid black",padding:"10px"}}>Reac(nv)</td>
+
+                  </tr>
+                  <tr>
+                    <td style={{ border:"1px solid black",padding:"10px"}}>9:00 AM - 10:00 AM</td>
+                    <td style={{ border:"1px solid black",padding:"10px"}}>Reac(nv)</td>
+                  <td style={{ border:"1px solid black",padding:"10px"}}>Reac(nv)</td>
+                  <td style={{ border:"1px solid black",padding:"10px"}}>Reac(nv)</td>
+                 
+                  <td style={{ border:"1px solid black",padding:"10px"}}>Reac(nv)</td>
+                  <td style={{ border:"1px solid black",padding:"10px"}}>Reac(nv)</td>
+
+                  </tr>
+                  <tr> 
+                    <td style={{ border:"1px solid black",padding:"10px"}}>10:00 AM - 11:00 AM</td>
+                    <td style={{ border:"1px solid black",padding:"10px"}} colSpan={5}>LUNCH BREAK</td>
+
+                  </tr>
+                  <tr>
+                    <td style={{ border:"1px solid black",padding:"10px"}}>9:00 AM - 10:00 AM</td>
+                    <td style={{ border:"1px solid black",padding:"10px"}} rowSpan={2}>Reac(nv)</td>
+                  <td style={{ border:"1px solid black",padding:"10px"}}rowSpan={2}>Reac(nv)</td>
+                  <td style={{ border:"1px solid black",padding:"10px"}}>Reac(nv)</td>
+                 
+                  <td style={{ border:"1px solid black",padding:"10px"}}rowSpan={2}>Reac(nv)</td>
+                  <td style={{ border:"1px solid black",padding:"10px"}}>Reac(nv)</td>
+
+                  </tr>
+                  <tr>
+                    <td style={{ border:"1px solid black",padding:"10px"}}>9:00 AM - 10:00 AM</td>
+                    
+              
+                  <td style={{ border:"1px solid black",padding:"10px"}}>Reac(nv)</td>
+                 
+                
+                  <td style={{ border:"1px solid black",padding:"10px"}}>Reac(nv)</td>
+
+                  </tr>
+                  <tr>
+                    <td style={{ border:"1px solid black",padding:"10px"}}>9:00 AM - 10:00 AM</td>
+                    <td style={{ border:"1px solid black",padding:"10px"}}>Reac(nv)</td>
+                  <td style={{ border:"1px solid black",padding:"10px"}}>Reac(nv)</td>
+                  <td style={{ border:"1px solid black",padding:"10px"}}>Reac(nv)</td>
+                 
+                  <td style={{ border:"1px solid black",padding:"10px"}}>Reac(nv)</td>
+                  <td style={{ border:"1px solid black",padding:"10px"}}>Reac(nv)</td>
+
+                  </tr>
+
+
+
+</tbody>
+                </table>
+                </center>
+            </div>
+        )
+    }
+}
+export default Timetable
--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import {createRoot } from 'react-dom/client'
 import {StrictMode } from 'react'
+import Timetable from './lec2412/timetable';
 // import Component1 from './lec2412/component1';
-import HtmlComponent from './lec2412/htmlcomponents';
+// import HtmlComponent from './lec2412/htmlcomponents';
 
 // 1. class based implementation
 // adding a coment 
@@ -12,7 +13,9 @@ import HtmlComponent from './lec2412/htmlcomponents';
 createRoot(document.getElementById('root')).render(
   <StrictMode>
    {/* <Component1/> */}
-   <HtmlComponent/>
-   <div>hello1</div>
+   {/* <HtmlComponent/> */}
+   <Timetable/>
+
+   
   </StrictMode>,
 )
